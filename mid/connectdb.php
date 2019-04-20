f970ec01bb273d2ba998ba97a1b02e1fbe919858
marshaldb <dcrackel@gmail.com>
2019-04-19 20:34:44 -0400

    Added Database security

--- mid/connectdb.php
+++ mid/connectdb.php
@@ -1,11 +1,12 @@
 <?php
-    $hostname="localhost:3306";
-    $username="marshaldbmidreal"; //marshaldbmidreal_authsite rocco_auths
-    $password="Cando@123"; //Dr4g0nMidrealm! Dr4g0n
-    $dbname="marshald_marshaldb"; //marshald_marshaldb rocco_auths
+    include('encrypt.php');
+    $config = parse_ini_file('config.txt');
+    $converter = new Encryption;
 
-    $con=mysql_connect($hostname,$username,$password);
-    mysql_select_db($dbname,$con) or die ("Cannot connect the Database");
+    $dbuser = $converter->decode($config['db_user']); 
+    $dbpass = $converter->decode($config['db_password']);    
+
+    $con=mysql_connect($config['db_host'],$dbuser,$dbpass);
+    mysql_select_db($config['db_name'],$con) or die ("Cannot connect the Database");
     mysql_query("SET NAMES 'utf8'",$con);
-
 ?>
